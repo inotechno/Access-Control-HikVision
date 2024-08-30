e4712a7246b339997773eebeb2d40771393eb753
inotechno <2inotechno0@gmail.com>
2024-08-30 12:27:50 +0700

    [update] Update JSON Package

--- logger.js
+++ logger.js
@@ -1,30 +1,28 @@
-// logger.js
-import winston from 'winston';
+import log4js from 'log4js';
 
-import DailyRotateFile from "winston-daily-rotate-file";
-
-// Buat transport untuk rotasi log berdasarkan ukuran file
-const transport = new DailyRotateFile({
-  filename: 'logs/access-control-%DATE%.log',
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: true, // Mengompres file log yang sudah usang
-  maxSize: '20m', // Maksimal ukuran file log sebelum dirotasi (misalnya 20MB)
-  maxFiles: '14d' // Simpan file log selama 14 hari
+// Konfigurasi log4js
+log4js.configure({
+  appenders: {
+    file: {
+      type: 'dateFile',
+      filename: 'logs/access-control.log',
+      pattern: '.yyyy-MM-dd',
+      compress: true,
+      daysToKeep: 14,
+      maxSize: '20MB'
+    },
+    console: {
+      type: 'stdout'
+    }
+  },
+  categories: {
+    default: {
+      appenders: ['file', 'console'],
+      level: 'info'
+    }
+  }
 });
 
-// Buat logger dengan transport rotasi
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} ${level}: ${message}`;
-    })
-  ),
-  transports: [
-    transport,
-    new winston.transports.Console() // Tetap log ke console jika diperlukan
-  ],
-});
+const logger = log4js.getLogger();
 
 export default logger;
